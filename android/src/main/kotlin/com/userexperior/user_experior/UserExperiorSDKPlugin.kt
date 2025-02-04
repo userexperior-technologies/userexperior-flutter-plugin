091b0a5a8e3e860ceb8a2b3fa8cb39aa3610d099
ArkadiGiniApps <arkadiy@gini-apps.com>
2025-02-04 13:15:37 +0200

    feature/ISS-132317-optimise-flutter-masking - update android conetnet

--- android/src/main/kotlin/com/userexperior/user_experior/UserExperiorSDKPlugin.kt
+++ android/src/main/kotlin/com/userexperior/user_experior/UserExperiorSDKPlugin.kt
@@ -7,19 +7,20 @@ import android.view.View
 import com.userexperior.bridge.location.UEPlatformMask
 import com.userexperior.bridge.model.UEPlatformPluginInformation
 import com.userexperior.bridge.model.UEPlatformPluginInterface
+import com.userexperior.bridge.model.UEPlatformPluginView
 import io.flutter.embedding.android.FlutterView
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
 import java.util.WeakHashMap
 
+@Suppress("unused")
 class UserExperiorSDKPlugin(
     private val methodChannels: HashMap<BinaryMessenger, MethodChannel>,
     private var recordingAllowed: Boolean,
 ) : UEPlatformPluginInterface {
 
     // region - attributes
-    private val locationsCache = WeakHashMap<FlutterView, WeakHashMap<String,UEPlatformMask>>()
-
+    private val cache = WeakHashMap<FlutterView, UEPlatformPluginView>()
     private var isDebugMode = false //with false to production
     // endregion
 
@@ -28,41 +29,65 @@ class UserExperiorSDKPlugin(
     }
 
     // region - UEPlatformPluginInterface
-    override fun isRecordingAllowed() : Boolean = recordingAllowed
-    override fun pluginInformation()  : UEPlatformPluginInformation =
+    override fun isRecordingAllowed(): Boolean = recordingAllowed
+    override fun pluginInformation(): UEPlatformPluginInformation =
         UEPlatformPluginInformation("FLUTTER", "5.0.0.1")
-    override fun pluginRootClasses()  : List<Class<out View?>?> = listOf(FlutterView::class.java)
-    override fun obtainMaskedLocationsData(instance: View?) : List<UEPlatformMask?> {
 
+    override fun pluginRootClasses(): List<Class<out View?>?> = listOf(FlutterView::class.java)
+    override fun obtainPluginView(instance: View): UEPlatformPluginView? {
         if (instance !is FlutterView) {
-            return ArrayList()
+            return super.obtainPluginView(instance)
         }
 
-        val methodChannel = methodChannels[instance.binaryMessenger] ?: return ArrayList()
+        val methodChannel =
+            methodChannels[instance.binaryMessenger] ?: return super.obtainPluginView(instance)
 
-        val timer = UserExperiorTimer()
-        timer.start()
+        val timer = UserExperiorTimer().apply { start() }
 
         Handler(Looper.getMainLooper()).post {
 
-            methodChannel.invokeMethod( "getMarkerLocations","arg", object : MethodChannel.Result {
+            methodChannel.invokeMethod("fetchFlutterData", mapOf("mode" to "full"), object : MethodChannel.Result {
+
                 override fun success(result: Any?) {
 
-                    val locations = result as? ArrayList<HashMap<String,String>> ?: return
+                    val cacheEntry = cache.getOrPut(instance) { UEPlatformPluginView() }
+                    val payload = (result as? HashMap<*, *>)
+                        ?.filterKeys { it is String }
+                        ?.mapKeys { it.key as String } as? HashMap<String, Any>
+                        ?: HashMap()
 
-                    val locationsForView = locationsCache[instance].let {
-                        var locationsForView = locationsCache[instance]
-                        if (locationsForView == null) {
-                            locationsForView = WeakHashMap<String,UEPlatformMask>()
-                            locationsCache[instance] = locationsForView
+                    val wireframe = payload["wireframe"] as? String
+                    val encodedImage = payload["screenshot"] as? ByteArray
+                    val encodedWidth = payload["width"] as? Int
+                    val encodedHeight = payload["height"] as? Int
+                    val encodedFormat = payload["format"] as? Int
+                    val locations = (payload["locations"] as? List<*>)
+                        ?.filterIsInstance<HashMap<String, String>>()
+                        ?.toCollection(ArrayList())
+                        ?: ArrayList()
+
+                    cacheEntry.locations.clear()
+                    if (locations.isNotEmpty()) {
+                        locations.forEach { location ->
+                            location.toUEPlatformMask().let { mask ->
+                                cacheEntry.locations[mask.identifier] = mask
+                            }
                         }
-                        locationsForView
                     }
-                    locationsForView.clear()
-
-                    for (location in locations) {
-                        val mask = location.toUEPlatformMask()
-                        locationsForView[mask.identifier] = mask
+                    if (encodedImage != null) {
+                        cacheEntry.encodedImage = encodedImage
+                    }
+                    if (encodedWidth != null) {
+                        cacheEntry.width = encodedWidth
+                    }
+                    if (encodedHeight != null) {
+                        cacheEntry.height = encodedHeight
+                    }
+                    if (encodedFormat != null) {
+                        cacheEntry.encodedFormat = encodedFormat
+                    }
+                    if (wireframe != null) {
+                        cacheEntry.wireframe = wireframe
                     }
 
                     if (isDebugMode) {
@@ -73,7 +98,9 @@ class UserExperiorSDKPlugin(
                 override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
                     Log.d(
                         "UserExperiorPlugin",
-                        "Error occurred on MaskedLocations, please submit a bug. Or check that you have added UEMarker Widget to your application"
+                        "Error occurred on MaskedLocations, please submit a bug.\n" +
+                                "Or check that you have added UEMarker Widget to your application\n" +
+                                "Code=$errorCode, Message=$errorMessage, Details=$errorDetails"
                     )
                 }
 
@@ -86,7 +113,33 @@ class UserExperiorSDKPlugin(
                 }
             })
         }
-        return locationsCache[instance]?.values?.toList() ?: ArrayList()
+        return cache[instance]
     }
     // endregion
+}
+
+private fun HashMap<String, String>.toUEPlatformMask(): UEPlatformMask {
+    val identifier = this["i"] ?: throw IllegalArgumentException("Identifier is required")
+    val x = this["x"]?.toFloatOrNull()
+        ?: throw IllegalArgumentException("x is required and must be a float")
+    val y = this["y"]?.toFloatOrNull()
+        ?: throw IllegalArgumentException("y is required and must be a float")
+    val w = this["w"]?.toFloatOrNull()
+        ?: throw IllegalArgumentException("width is required and must be a float")
+    val h = this["h"]?.toFloatOrNull()
+        ?: throw IllegalArgumentException("height is required and must be a float")
+    return UEPlatformMask(identifier, x, y, w, h)
+}
+
+private class UserExperiorTimer {
+    private var start: Long = 0L
+
+    fun start() {
+        start = System.nanoTime()
+    }
+
+    fun elapsedMicros(): Long {
+        val current = System.nanoTime()
+        return (current - start) / 1000
+    }
 }
