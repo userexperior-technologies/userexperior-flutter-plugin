4474086f574563481e2d25dd59173e20cd2e59ef
ArkadiGiniApps <arkadiy@gini-apps.com>
2025-02-14 15:28:29 +0200

    feature/ISS-132317-optimise-flutter-repaint - added a key to the RepaintBoundary - should improve behaviour

--- lib/src/ue_recording.dart
+++ lib/src/ue_recording.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+/// Key which is used to identify the [RepaintBoundary]
+final _ueRecordingWidgetKey = GlobalKey(debugLabel: 'ue_recording_widget');
+
 class UERecordingWidget extends StatelessWidget {
   final Widget child;
+  GlobalKey get _sk => _ueRecordingWidgetKey;
 
   const UERecordingWidget({super.key, required this.child});
 
   @override
-  Widget build(BuildContext context) => RepaintBoundary(child: child);
+  Widget build(BuildContext context) => RepaintBoundary(key: _sk, child: child);
 }
